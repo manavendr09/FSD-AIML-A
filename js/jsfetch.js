8b178c0e52862d01625218778cb8d58c6dfeed19
manavendr09 <manavendrayadav2617@gmail.com>
2025-09-18 10:24:44 +0530

    fetch git data

---
+++ js/jsfetch.js
@@ -0,0 +1,12 @@
+const url="https://api.github.com/users/manavendr09"
+const res=fetch(url);
+res.then((response)=>{
+  if(!response.ok){
+    throw new Error("Network response was not ok");
+  }
+  return response.json();
+}).then((data)=>{
+  console.log("DATA=",data);
+}).catch((error)=>{
+  console.error("There was a problem with the fetch operation:", error);
+});
